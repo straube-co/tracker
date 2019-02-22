ac6eaef5ccb75a688c8ea34c58b0f39c67c7cdfe
Lucas <lucas@straube.co>
2019-02-22 14:38:41 -0300

    Starting project

---
+++ app/Console/Commands/Update.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Project;
+use App\Task;
+
+class Update extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    private $client;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+         $this->info("Building!");
+
+         $client = $this->getClient();
+         $projects = $client->projects->findByWorkspace(870874468980849);
+
+         foreach ($projects as $project) {
+             $count = \App\Project::where('id', $project->id)->count();
+
+             if ($count === 0) {
+                 Project::create([
+                     'id' => $project->id,
+                     'name' => $project->name,
+                 ]);
+             }
+
+             $this->importTasks($project);
+         }
+    }
+
+    public function importTasks($project)
+    {
+        $client = $this->getClient();
+        $tasks = $client->tasks->findByProject($project->id);
+
+        foreach ($tasks as $task) {
+            Task::create([
+                'id' => $task->id,
+                'name' => $task->name,
+                'project_id' => $project->id,
+            ]);
+        }
+    }
+
+    public function getClient()
+    {
+        if (!isset($this->client)) {
+            $this->client = \Asana\Client::accessToken('0/b41a58e0103d856760d375e247e8dd13');
+        }
+        return $this->client;
+    }
+}
---
+++ database/migrations/2019_02_22_145909_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigInteger('id');
+            $table->string('name', 100);
+            $table->bigInteger('project_id');
+            $table->timestamps();
+            $table->foreign('project_id')->references('id')->on('projects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
