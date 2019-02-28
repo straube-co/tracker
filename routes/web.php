47c6843b12b216c7c47c485deaec4f29470daf38
Lucas <lucas@straube.co>
2019-02-28 15:54:52 -0300

    Creating routes.

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ Route::get('/', function () {
 });
 
 Route::get('/auth', 'Auth\\OAuthController@auth')->name('auth.auth');
-Route::get('/auth/handle', 'Auth\\OAuthController@handle')->name('auth.handle'); //
+Route::get('/auth/handle', 'Auth\\OAuthController@handle')->name('auth.handle'); 
 Route::resource('/time', 'TimeController');
