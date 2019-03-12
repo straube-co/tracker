fbe44f77c6ab79d9f6b143e690571a1ba43f2fc0
Lucas <lucas@straube.co>
2019-03-12 18:27:18 -0300

    Updating the ImportController for execute a csv import.

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon;
 use App\Activity;
 use App\Project;
 use App\Task;
@@ -13,14 +14,40 @@ class ImportController extends Controller
 {
     public function index()
     {
-        return view('import.import');
+        $projects = Project::get();
+
+        $data = [
+            'projects' => $projects,
+        ];
+
+        return view('import.import', $data);
     }
 
-    public function import(Request $request)
+    public function store(Request $request)
     {
-        $file = $request->file('import_file')->openFile();
+        $file = $request->file('import_file')->openFile();                      //abrindo arquivo que está sendo importado.
+        $first = true;
+
         while (!$file->eof()) {
-            echo $file->fgetcsv();
+            $line = $file->fgetcsv();                                           //exibindo o arquivo uma linha por vez dentro do while.
+
+            if ($first) {
+                $first = false;                                                 //ignorando a primeira linha e continuando.
+                continue;
+            }
+
+            $count = \App\Task::where('name', $file[0])->count();
+
+            if($count ===  0){
+
+                Time::create([
+                    'project_id' => $request->project_id,                          //recebendo project_id do select.
+                    'user_id' => $request->session()->get('auth.id'),
+                    'activity_id' => 5,                                            //associando activit_id com o id de "OUTRAS" nas atividades.
+                    'started' => Carbon\Carbon::parse($line[6]),
+                    'finished' => Carbon\Carbon::parse($line[7]),                  //acesando a linha [5]e[6] do array e passando o formato esperado de data.
+             ]);
+            }
         }
     }
 }
--- resources/views/import/import.blade.php
+++ resources/views/import/import.blade.php
@@ -1,9 +1,20 @@
 @extends('layouts.header')
 
 @section('content')
-    <form method="post">
-        <input type="file" name="import_file"/>
-        <br>
+    <form action="{{ route('import.store') }}" enctype="multipart/form-data" method="post">
+        {{ csrf_field() }}
+        <div>
+            <input type="file" name="import_file"/>
+            <br>
+            <br>
+            <label for="project">Project: </label>
+            <select id="project" name="project_id">
+                <option value="">Select</option>
+                @foreach ($projects as $project)
+                <option value="{{ $project->id }}">{{ $project->name }}</option>
+                @endforeach
+            </select>
+        </div>
         <br>
         <button type="submit" style="background-color: lightsteelblue;" class="btn btn-primário">Importar arquivo </button>
     </form>
