dbd62260c968c1c2d38b35f9864f9d1b27da7caa
Gustavo Straube <gustavo@creativeduo.com.br>
2019-07-01 11:21:40 -0300

    Added CSV format option to shared report.

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Carbon\Carbon;
 use App\Report;
+use App\Support\Formatter;
 use App\Time;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 /**
  *
@@ -13,31 +14,14 @@ use App\Time;
  */
 class ShareController extends Controller
 {
-    public function show(Report $report)
+    public function show(Report $report, string $format = null)
     {
-        //
-        $query = Time::orderBy('started', 'desc');
+        $format = in_array($format, [ 'html', 'csv' ]) ? $format : 'html';
 
-        if (($activity = $report->filter['activity_id'])) {
-            $query->where('activity_id', $activity);
-        }
-        if (($project = $report->filter['project_id'])) {
-            $query->whereHas('task', function ($query) use ($project) {
+        $query = $this->buildQuery($report);
 
-                $query->where('project_id', $project);
-            });
-        }
-        if (($task = $report->filter['task_id'])) {
-            $query->where('task_id', $task);
-        }
-        if (($user = $report->filter['user_id'])) {
-            $query->where('user_id', $user);
-        }
-        if (($started = $report->filter['started'])) {
-            $query->where('started', '>=', $started);
-        }
-        if (($finished = $report->filter['finished'])) {
-            $query->where('finished', '<=', $finished);
+        if ($format === 'csv') {
+            return $this->getCsv($query);
         }
 
         $summaryQuery = clone $query;
@@ -64,5 +48,76 @@ class ShareController extends Controller
         ];
 
         return view('report.share', $data);
+    }
+
+    private function buildQuery(Report $report)
+    {
+        $query = Time::with('activity', 'task', 'task.project')->orderBy('started', 'desc');
+
+        if (($activity = $report->filter['activity_id'])) {
+            $query->where('activity_id', $activity);
+        }
+        if (($project = $report->filter['project_id'])) {
+            $query->whereHas('task', function ($query) use ($project) {
+
+                $query->where('project_id', $project);
+            });
+        }
+        if (($task = $report->filter['task_id'])) {
+            $query->where('task_id', $task);
+        }
+        if (($user = $report->filter['user_id'])) {
+            $query->where('user_id', $user);
+        }
+        if (($started = $report->filter['started'])) {
+            $query->where('started', '>=', $started);
+        }
+        if (($finished = $report->filter['finished'])) {
+            $query->where('finished', '<=', $finished);
+        }
+
+        return $query;
+    }
+
+    private function getCsv($query)
+    {
+        $rows = [
+            [
+                'Project',
+                'Task',
+                'Activity',
+                'User',
+                'Started',
+                'Finished',
+                'Ellapsed time',
+            ]
+        ];
+
+        $results = $query->get();
+
+        foreach ($results as $time) {
+            $ellapsed = null;
+            if ($time->finished) {
+                $ellapsed = Formatter::intervalTime(
+                    $time->finished->diffAsCarbonInterval($time->started)
+                );
+            }
+            $rows[] = [
+                $time->task->project->name,
+                $time->task->name,
+                $time->activity->name,
+                $time->user->name,
+                $time->started_at,
+                $time->finished_at,
+                $ellapsed,
+            ];
+        }
+
+        $rows = array_map(function ($row) {
+            return '"' . implode('","', $row) . '"';
+        }, $rows);
+
+        return response(implode("\n", $rows), 200)
+              ->header('Content-Type', 'text/csv');
     }
 }
