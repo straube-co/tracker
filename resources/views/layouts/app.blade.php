0033b9f736ecef7922f33d0bce8a25dd2eea9537
Gustavo Straube <gustavo@creativeduo.com.br>
2020-07-06 19:01:31 -0300

    Minor code styling change.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,13 +71,15 @@
                                         Users
                                     </a>
                                     <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                    <a
+                                        class="dropdown-item"
+                                        href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();document.getElementById('logout-form').submit();"
+                                    >
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none;">
                                         @csrf
                                     </form>
                                 </div>
