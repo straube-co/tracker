b46aded3a464d591430c0c021ee3fd01279f92ab
Gustavo Straube <gustavo@amp.build>
2021-10-30 12:48:49 -0300

    Moved time decimal option to a config file.

---
+++ config/tracker.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Time Decimal
+    |--------------------------------------------------------------------------
+    |
+    | This option controls how time should be rendered in the app and reports 
+    | as decimal of hours, instead of hours and minutes. If it is set to 
+    | "true", 1 hour and 14 minutes will be rendered as 1.23 instead of 1:14.
+    |
+    | Changing this option value only affects rendering and not how values are  
+    | stored in the database.
+    */
+
+    'time_decimal' => false,
+
+];
